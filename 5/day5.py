1592824314ce88f05814e9de1411e7681111e1a2
Wouter <wouter075@gmail.com>
2018-12-14 21:03:50 +0100

    Small update on 5.1

--- 5/day5.py
+++ 5/day5.py
@@ -1,12 +1,8 @@
 poly = open("input.txt").read()
-
 for y in range(2000):
     for x in range(len(poly)):
         if x > 0:
             comp = poly[x:x+2]
-            if len(comp) == 2:
-                if comp[0] != comp[1]:
-                    if comp[0].upper() == comp[1].upper():
+            if (len(comp) == 2) and (comp[0] != comp[1]) and (comp[0].upper() == comp[1].upper()):
                         poly = poly.replace(comp, "")
                         print("match!" + comp + str(len(poly)))
-
